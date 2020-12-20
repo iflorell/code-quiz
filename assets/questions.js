0f16ca11018c710889efd2b0e0ad4c1c2769d9c6
Ike Flo <isaac.florell@gmail.com>
2020-12-19 17:02:33 -0700

    begin array for questions

---
+++ assets/questions.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "object", "alerts", " undefined","numbers", "boolean"],
+        answer: "alerts"
+    },
+    
+    {
+        title: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
